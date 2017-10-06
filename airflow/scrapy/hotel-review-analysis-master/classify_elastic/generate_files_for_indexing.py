344bf0ad963493cc0c0b7a73634178b9b9a53ebf
SerDiaz <sdiazgon@ull.edu.es>
2017-10-06 13:19:16 +0100

    Fixed up generation files to index

--- airflow/scrapy/hotel-review-analysis-master/classify_elastic/generate_files_for_indexing.py
+++ airflow/scrapy/hotel-review-analysis-master/classify_elastic/generate_files_for_indexing.py
@@ -2,14 +2,25 @@ import unicodecsv as csv
 import hashlib
 import sys
 from opinionTokenizer import tokenize_into_opinion_units
+#import nltk
+#nltk.download('punkt')
 
 filename=sys.argv[1]
 f = open(filename)
 samples = [row for row in csv.reader(f)]
 samples[0].append("key")
+review_date_position=0
+key_position=len(samples[0])-1
+print(samples[0][9])
+
+i=0
+for field in samples[0]:
+  if field=='review_date':
+    review_date=i
+  i=i+1
 
 for i in range(1,len(samples)):
-    key =  hashlib.md5(samples[i][5].encode('utf-8')).hexdigest()
+    key =  hashlib.md5(samples[i][review_date_position].encode('utf-8')).hexdigest()
     samples[i].append(key)
 
 #write the reviews with the keys, this file will be used for indexing
@@ -17,10 +28,10 @@ with open('keys_' + filename, 'wb') as csvfile:
         writer = csv.writer(csvfile, dialect='excel')
         writer.writerows(samples)
 
-
+'''
 opinion_units = []
 for i in range(1,len(samples)):
-    key = samples[i][12]
+    key = samples[i][key_position]
     for opinion_unit in tokenize_into_opinion_units(samples[i][4]) + tokenize_into_opinion_units(samples[i][5]):
         opinion_units.append([key, opinion_unit])
 
@@ -28,4 +39,4 @@ for i in range(1,len(samples)):
 with open('opinion_units_keys_' + filename, 'wb') as csvfile:
         writer = csv.writer(csvfile, dialect='excel')
         writer.writerows(opinion_units)
-
+'''
