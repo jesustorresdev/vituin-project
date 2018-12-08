5b0e688b703146615131c4272c31e40a3e7487e4
VITUIN Project <vituin@ull.edu.es>
2018-12-08 16:41:49 +0000

    En index seguridad no tiene sentido quitar la mayuscula

--- airflow/data_analysis/classify_elastic/structured_data/index_factores_económicos/index_seguridad_robos.py
+++ airflow/data_analysis/classify_elastic/structured_data/index_factores_económicos/index_seguridad_robos.py
@@ -30,6 +30,6 @@ attributes_to_fixed={
     "type": "Robos",
 }
 
-index_excels_Istac.main(excel, sheet, name_index, type_index, name_items, table_start_and_end, type_value, attributes_to_fixed = attributes_to_fixed, fields_to_change = fields_to_change)
+index_excels_Istac.main(excel, sheet, name_index, type_index, name_items, table_start_and_end, type_value, attributes_to_fixed = attributes_to_fixed)
 
 
