77ea2a6c7d296f0fcf09e8d6d08a7e8a1111e392
VITUIN Project <vituin@ull.edu.es>
2018-12-10 13:01:23 +0000

    Ahora redes sociales extraen de los csv. Se borran también los excels anteriores

--- airflow/data_analysis/classify_elastic/structured_data/index_redes_sociales/index_Facebook_likealyzer.py
+++ airflow/data_analysis/classify_elastic/structured_data/index_redes_sociales/index_Facebook_likealyzer.py
@@ -1,66 +1,26 @@
-# -*- coding: utf-8 -*-
-import sys
-sys.path.append('../')
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+import utils
 
-from structured_data import index_excels_normalTable
+import unicodecsv as csv
+import datetime
 
-file =  sys.argv[1]
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 
+#takes two arguments:
+#   the name of the file to index
+#   the starting index for the id
+#       the ids shouldn't overlap or you will replace existing opinion units
+#IMPORTANT: before indexing opinion units you must index the parent reviews
 
-excel = file
-sheet = 0
-name_index = "index_facebook_likealyzer"
-type_index = "structured"
-
-
-table_start_and_end = {
-    "start_row": 0,
-    "start_col": 0,
-    "end_row": 2,
-    "end_col": 33,
-    "start_value_row": 2,
-    "start_value_col": 0
-}
-
-type_items = {
-    "about" : int,
-    "aboutAvailable" : str,
-    "achievementsQuality" : str,
-    "activity" : int,
-    "answerToUserResponseTime" : str,
-    "answerToUsersRatio" : str,
-    "comments" : str,
-    "contactInfoAvailable" : str,
-    "dailyMessages" : float,
-    "date" : str,
-    "emailAvailable" : str,
-    "engagement" : int,
-    "frontPage" : int,
-    "likedPages" : int,
-    "locationAvailable" : str,
-    "messageLengthRatio" : int,
-    "name" : str,
-    "originalFBVideos" : int,
-    "participationRatio" : str,
-    "peopleTalking" : int,
-    "percentageOfNotes" : int,
-    "percentageOfPhotos" : int,
-    "percentageOfVideos" : int,
-    "phoneAvailable" : str,
-    "platform" : str,
-    "response" : str,
-    "source" : str,
-    "summary" : str,
-    "totalPageLikes" : int,
-    "userPhotoAvailable" : str,
-    "usernameAvailable" : str,
-    "usersCanPost" : str,
-    "websiteAvailable" : str,
-}
+filename =  sys.argv[1]
 
 
-
-name_items = [
+F = open(filename)
+reference = [
     "about",
     "aboutAvailable",
     "achievementsQuality",
@@ -96,10 +56,100 @@ name_items = [
     "websiteAvailable",
 ]
 
-date=['date']
+ES = Elasticsearch(
+    [
+        'elasticsearch:9200/'
+    ]
+)
+
+FILE_COUNT = 0
+ACTIONS = []
+
+EXIST_INDEX = True
+FIRST_ITERATION = False
+ELASTICSEARCH_INDEX='index_facebook_likealyzer'
+ELASTICSEARCH_DOC_TYPE='structured'
+NAMES_ITEM_FINAL = []
+
+#Search the last indexed id
+doc = {
+    'query': {
+        'match_all' : {}
+    }
+}
+
+try:
+    res = ES.search(index=ELASTICSEARCH_INDEX, body=doc, size=0)
+    #The next element indexed going to be the next id doesn't used
+    cont_id = int(res['hits']['total'])
+
+except:
+    #If it's the first gruop of elements indexed
+    print("First indexed")
+    cont_id = 0
+    EXIST_INDEX = False
+    FIRST_ITERATION = True
+
+j = 0
+
+for row in csv.reader(F):
+    j +=1
+    if(FILE_COUNT!=0):
+        item = {}
+        try:
+            for i in range(len(reference)):
+                item[reference[i]] = row[i]
+            item = utils.set_date(item,['date'])
+            res ={}
+            try:
+                res = ES.search(index=ELASTICSEARCH_INDEX, body={
+                    "query": {
+                        "bool": {
+                            "must":[
+                                {"term":{"date": item['date']}}, #Use the key to compare
+                                {"term":{"name": item['name']}}, #Use the key to compare
+                            ]
+                        }
+                    }
+                })
+            except:
+                pass
+
+            if not res or res['hits']['hits'] == []: #If there isn't result
+
+                if not EXIST_INDEX and FIRST_ITERATION is True:
+                    NAMES_ITEM_FINAL = utils.get_names_item_final(item)
+                    FIRST_ITERATION=False
+
+                item['upload_date']=datetime.datetime.today()
+
+                action = {
+                    "_index": ELASTICSEARCH_INDEX,
+                    "_type": ELASTICSEARCH_DOC_TYPE,
+                    "_id": cont_id,
+                    "_source": item
+                }
+                ACTIONS.append(action)
+
+                cont_id += 1
+            else:
+                print 'repeat ', FILE_COUNT
+
+        except Exception as error:
+            print 'Error', error, ', en', FILE_COUNT
+            print '------'
+            pass
+
+    FILE_COUNT += 1
 
 
-index_excels_normalTable.main(excel, sheet, name_index, type_index, table_start_and_end, type_items, name_items, date=date)
 
-
+if FILE_COUNT > 0:
+    if EXIST_INDEX is False:
+        es_new = utils.set_properties(NAMES_ITEM_FINAL, ELASTICSEARCH_DOC_TYPE, ELASTICSEARCH_INDEX)
+        helpers.bulk(es_new, ACTIONS)
+    else:
+        helpers.bulk(ES, ACTIONS)
+    print "leftovers"
+    print "indexed %d" %cont_id
 
